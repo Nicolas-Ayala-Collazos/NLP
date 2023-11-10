71c0f0e9c2cd126a8b771073b517b1c5452d8a38
Nicolas <147957529+Nicolas-Ayala-Collazos@users.noreply.github.com>
2023-11-10 02:29:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,38 @@
-
 import tensorflow as tf
+
+# Path to the saved_model.pb file
+saved_model_path = 'path_to_your_saved_model_directory'
+
+# Load the SavedModel
+loaded_model = tf.saved_model.load(saved_model_path)
+
+# List out the available signatures for inference
+signatures = list(loaded_model.signatures.keys())
+
+# Assuming 'serving_default' is your signature key of interest
+inference = loaded_model.signatures['serving_default']
+
+# Now to integrate it with Streamlit, here's a starter code:
 import streamlit as st
-import numpy as np
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 
-# Cargar el modelo y el tokenizer
-#model = TFAutoModelForSequenceClassification.from_pretrained('/saved_model_BERT_multilingual.pb')
-#tokenizer = AutoTokenizer.from_pretrained('bert-base-multilingual-cased')
+st.title("Noticias Veracidad Checker")
 
-# Función para predecir la veracidad de la noticia
-#def predict_veracity(statement):
- #   inputs = tokenizer(statement, return_tensors="tf", padding=True)
-  #  outputs = model(inputs)[0]
-  #  probabilities = tf.nn.softmax(outputs, axis=1)
-  #  prediction = np.argmax(probabilities, axis=1)
-  #  return prediction
+# Text input for the news statement
+statement = st.text_area("Ingresa la afirmación para verificar su veracidad:")
 
-# Crear la interfaz de usuario con Streamlit
-st.title('Clasificador de Noticias con BERT')
-statement = st.text_area("Introduce la afirmación para verificar:")
+# Button to trigger prediction
+if st.button("Verificar"):
+    if statement:
+        # Preprocessing and tokenization steps would go here
+        # For the sake of this example, let's assume the model takes tokenized text
+        tokenized_text = tokenize(statement)  # replace 'tokenize' with your actual function
 
-#if st.button('Verificar'):
- #   prediction = predict_veracity(statement)
-  #  st.write(f'La noticia es: {"Verdadera" if prediction else "Falsa"}')
+        # Run the model's inference
+        result = inference(tf.constant([tokenized_text]))  # adjust according to the model's expected input
+
+        # Postprocess the results and display
+        # Here you would convert the model output to a human-readable form
+        prediction = result['output_0'].numpy()[0]  # adjust 'output_0' to your actual output key
+        st.write("La afirmación es:", "Verdadera" if prediction > 0.5 else "Falsa")
+    else:
+        st.write("Por favor, ingresa una afirmación para verificar.")
