0c035a13ac7f6277dc716462425c7f7d2041aa73
Nicolas <147957529+Nicolas-Ayala-Collazos@users.noreply.github.com>
2023-11-10 02:22:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,21 @@ import numpy as np
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 
 # Cargar el modelo y el tokenizer
-model = TFAutoModelForSequenceClassification.from_pretrained('/saved_model_BERT_multilingual.pb')
-tokenizer = AutoTokenizer.from_pretrained('bert-base-multilingual-cased')
+#model = TFAutoModelForSequenceClassification.from_pretrained('/saved_model_BERT_multilingual.pb')
+#tokenizer = AutoTokenizer.from_pretrained('bert-base-multilingual-cased')
 
 # Función para predecir la veracidad de la noticia
-def predict_veracity(statement):
-    inputs = tokenizer(statement, return_tensors="tf", padding=True)
-    outputs = model(inputs)[0]
-    probabilities = tf.nn.softmax(outputs, axis=1)
-    prediction = np.argmax(probabilities, axis=1)
-    return prediction
+#def predict_veracity(statement):
+ #   inputs = tokenizer(statement, return_tensors="tf", padding=True)
+  #  outputs = model(inputs)[0]
+  #  probabilities = tf.nn.softmax(outputs, axis=1)
+  #  prediction = np.argmax(probabilities, axis=1)
+  #  return prediction
 
 # Crear la interfaz de usuario con Streamlit
 st.title('Clasificador de Noticias con BERT')
 statement = st.text_area("Introduce la afirmación para verificar:")
 
-if st.button('Verificar'):
-    prediction = predict_veracity(statement)
-    st.write(f'La noticia es: {"Verdadera" if prediction else "Falsa"}')
+#if st.button('Verificar'):
+ #   prediction = predict_veracity(statement)
+  #  st.write(f'La noticia es: {"Verdadera" if prediction else "Falsa"}')
